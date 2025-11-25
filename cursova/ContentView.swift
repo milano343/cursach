0c314e479542678cfb33d78287e924b7e49ad17e
UNFU <UNFU@email.com>
2025-11-25 10:32:51 +0200

    Initial Commit

---
+++ cursova/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cursova
+//
+//  Created by stud on 25.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cursova/cursovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursovaApp.swift
+//  cursova
+//
+//  Created by stud on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct cursovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
